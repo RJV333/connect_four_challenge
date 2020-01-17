2d768a91cd05d0d7e5cc3142b4518e6d40ec4397
is <isaac.ball@annkissam.com>
2020-01-17 12:17:38 -0500

    Sorry :)
    [ci skip]

--- game_script.rb
+++ game_script.rb
@@ -10,12 +10,12 @@ set title: 'Connect Four Game Player', background: 'white'
 g = Grid.new
 
 tick = 0 # set timer to 0
-time_increment = 80 # approx 2 seconds per move
+time_increment = 120 # approx 2 seconds per move
 
-p1 = AquaTaco.new
+p1 = RandomPlayer.new
 p2 = EasyPlayer.new
-p1.color = 'aqua'
-p2.color = 'red'
+p1.color = 'red'
+p2.color = 'olive'
 p1.opponent = p2.color
 p2.opponent = p1.color
 
